a078809788a702678b4169a4166a3355c1e29e94
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-12-24 12:19:38 +0100

    Require Cucumber Rails' NullStrategy

--- features/support/rails.rb
+++ features/support/rails.rb
@@ -8,6 +8,16 @@ require "cucumber/rails/database/strategy"
 require "cucumber/rails/database/deletion_strategy"
 require "cucumber/rails/database/shared_connection_strategy"
 require "cucumber/rails/database/truncation_strategy"
+
+# FIXME: NullStrategy is a new strategy introduced in a cucumber-rails version
+# that has not been released yet. Remove this begin-rescue block and bump
+# the cucumber-rails dependency in other gemfiles when a stable version will
+# be released
+begin
+  require "cucumber/rails/database/null_strategy"
+rescue LoadError
+end
+
 require "cucumber/rails/database"
 
 MultiTest.disable_autorun
